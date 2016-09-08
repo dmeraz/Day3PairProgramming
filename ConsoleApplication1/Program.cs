de803ed6faa5ac208855a2ed3731457d2bb5e515
dmeraz <darin.meraz@gmail.com>
2016-09-08 14:26:42 -0400

    Pranav's run

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lisa's muffins
+            int recipecups = 7;
+            int cupsadded = 2;
+
+            Console.WriteLine(recipecups - cupsadded);
+        }
+    }
+}
