cde1b0c0dd2312054e9668173b0c2cbc0b9491e8
dmeraz <darin.meraz@gmail.com>
2016-09-08 16:59:21 -0400

    first commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,10 +11,29 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Lisa's muffins
-            int recipecups = 7;
-            int cupsadded = 2;
+            //int recipecups = 7;
+            //int cupsadded = 2;           
+            //Console.WriteLine(recipecups - cupsadded);
 
-            Console.WriteLine(recipecups - cupsadded);
+            //int diners = 4;
+            //int billShare = 13;
+            //Console.WriteLine(diners * billShare); 
+
+            //Diapers
+            //int diaperPack = 8;
+            //int budget = 40;
+            //Console.WriteLine(budget / diaperPack);
+
+            //int oldBalance = 29;
+            //int newBalance = 41;
+            //int moneyReceived = (newBalance - oldBalance); 
+            //Console.WriteLine(moneyReceived);
+
+            //Pranav's run
+            int Juliaran = 47;
+            int Pranavran = (Juliaran - 30);
+            Console.WriteLine(Pranavran);
+
         }
     }
 }
